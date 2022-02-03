0ee1a36af00b40789f695942cfa366554ac4914c
ltstucki <inertiastooks@gmail.com>
2022-02-02 22:03:08 -0700

    Added a Person Class and parachute
    The parachute has functions in itself that only need to be called every time a word is guessed wrong for example
    
    break_chute() will be called on every wrong guess

---
+++ Parachute_game/__main__.py
@@ -0,0 +1,34 @@
+#This main file can be removed, it is just to show how the parachute works and is in fully functioning condition.
+
+from person import Person
+from parachute import Parachute
+person = Person()
+
+parachute = Parachute()
+#Shows the parachute working and at full capacity
+parachute.show_chute()
+person.draw_person()
+parachute.break_chute()
+
+#Uncomment these lines to see parachute break and what happens when all the parts are "torn"
+"""
+parachute.show_chute()
+person.draw_person()
+parachute.break_chute()
+
+parachute.show_chute()
+person.draw_person()
+parachute.break_chute()
+
+parachute.show_chute()
+person.draw_person()
+parachute.break_chute()
+
+
+parachute.show_chute()
+parachute.break_chute()
+"""
+
+
+
+
