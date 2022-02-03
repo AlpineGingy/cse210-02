252dfbe9980c009f786e2c71c85800b552089f60
SlenderSlayer24 <86080078+SlenderSlayer24@users.noreply.github.com>
2022-02-03 16:13:17 -0700

    Added testing of the word display

--- Parachute_game/__main__.py
+++ Parachute_game/__main__.py
@@ -4,12 +4,26 @@ from person import Person
 from parachute import Parachute
 from display import Display
 person = Person()
-
+display = Display()
 parachute = Parachute()
+#sample word
+sample = ["h","e","l","l","o"]
 #Shows the parachute working and at full capacity
 parachute.show_chute()
 person.draw_person()
+#this currently has a test word, it should be replaced by the random word form the get word program
+anwser = display.set_blanks(sample)
+'''the anwser should change based on user input and the blanks will be replaced with letters and the
+anwser should be reinputed into word_display for proper display in the console.
+'''
+display.word_display(anwser)
+#just for display, remove for full game
+print()
+print("correct anwser display")
+#just for display, remove above for full game
+display.word_display(sample)
 parachute.break_chute()
+
 
 #Uncomment these lines to see parachute break and what happens when all the parts are "torn"
 """
